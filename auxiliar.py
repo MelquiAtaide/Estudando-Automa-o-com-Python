405ac44f6813fdabeaa925ccedf334de01dc66f7
MelquiAtaide <melqueataide@gmail.com>
2024-03-07 22:21:31 -0300

    Finalizando o for e condição

--- auxiliar.py
+++ auxiliar.py
@@ -3,3 +3,5 @@ import time
 
 time.sleep(5)
 print(pyautogui.position())
+
+pyautogui.scroll(5000)
--- codigo.py
+++ codigo.py
@@ -1,25 +1,61 @@
-import pyautogui
+import pyautogui as py
 import time
 import pandas
 
-pyautogui.PAUSE = 0.5
+py.PAUSE = 0.5
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 tabela = pandas.read_csv("produtos.csv")
 
-pyautogui.press("win")
-pyautogui.write("edge")
-pyautogui.press("enter")
+py.press("win")
+py.write("edge")
+py.press("enter")
 
-pyautogui.write(link)
-pyautogui.press("enter")
+py.write(link)
+py.press("enter")
 
 time.sleep(2)
 
-pyautogui.click(x=558, y=353)
-pyautogui.write("python@gmail.com")
-pyautogui.press("tab")
-pyautogui.write("123456789")
-pyautogui.press("enter")
+py.click(x=558, y=353)
+py.write("python@gmail.com")
+py.press("tab")
+py.write("123456789")
+py.press("enter")
 time.sleep(2)
 
-pyautogui.click(x=580, y=238)
+
+for i in tabela.index:
+    py.click(x=580, y=238)
+
+    # Código do produto
+    py.write(tabela.loc[i, "codigo"])
+    py.press("tab")
+
+    # Marca
+    py.write(tabela.loc[i, "marca"])
+    py.press("tab")
+
+    # Tipo
+    py.write(tabela.loc[i, "tipo"])
+    py.press("tab")
+
+    # categoria
+    py.write(str(tabela.loc[i, "categoria"]))
+    py.press("tab")
+
+    # Preço
+    py.write(str(tabela.loc[i, "preco_unitario"]))
+    py.press("tab")
+
+    # Custo
+    py.write(str(tabela.loc[i, "custo"]))
+    py.press("tab")
+
+    # Obs
+    obs = tabela.loc[i, "obs"]
+    if not pandas.isna(obs):
+        py.write(obs)
+    py.press("tab")
+
+    #Enter
+    py.press("enter")
+    py.scroll(5000)
